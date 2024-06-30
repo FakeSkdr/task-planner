4325f41ee38e71f4dbbfc7ddbc17e05a04a9263a
Bastien <FakeSkdr@users.noreply.github.com>
2024-06-30 19:56:42 +0200

    fix(api): see if the bug is from reading the file

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -11,5 +11,44 @@ function sleep(ms: number) {
 export async function GET(request: Request) {
   // await sleep(2000);
 
-  return NextResponse.json(initialData, { status: 200 });
+  return NextResponse.json(
+    {
+      tasks: {
+        "task-1": {
+          id: "task-1",
+          title: "Task 1",
+          description: "Description 1",
+        },
+        "task-2": {
+          id: "task-2",
+          title: "Task 2",
+          description: "Description 2",
+        },
+        "task-3": {
+          id: "task-3",
+          title: "Task 3",
+          description: "Description 3",
+        },
+      },
+      columns: {
+        "column-1": {
+          id: "column-1",
+          title: "To do",
+          taskIds: ["task-1", "task-2", "task-3"],
+        },
+        "column-2": {
+          id: "column-2",
+          title: "In Progress",
+          taskIds: [],
+        },
+        "column-3": {
+          id: "column-3",
+          title: "Done",
+          taskIds: [],
+        },
+      },
+      columnOrder: ["column-1", "column-2", "column-3"],
+    },
+    { status: 200 },
+  );
 }
